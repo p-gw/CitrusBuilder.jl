01f7bf6fcffa14998b32689004436ff2fd49dd0d
p-gw <p.gewessler@gmx.at>
2022-05-12 12:59:48 +0200

    add docs

--- src/QuestionTypes.jl
+++ src/QuestionTypes.jl
@@ -4,6 +4,11 @@
     subquestion::AbstractString
 end
 
+"""
+    subquestion(; code, subquestion)
+
+Construct a LimeSurvey Subquestion.
+"""
 subquestion(; kwargs...) = SubQuestion(; kwargs...)
 
 struct ResponseOption <: AbstractQuestion
@@ -11,6 +16,11 @@ struct ResponseOption <: AbstractQuestion
     option::AbstractString
 end
 
+"""
+    response_option(; code, option)
+
+Construct a LimeSurvey Response Option.
+"""
 response_option(; code, option) = ResponseOption(code, option)
 
 struct ResponseScale
@@ -18,12 +28,51 @@ struct ResponseScale
     options::Vector{ResponseOption}
 end
 
+"""
+    response_scale(; options::Vector{ResponseOption}, header::AbstractString)
+    response_scale(children; header::AbstractString)
+
+Construct a LimeSurvey Response Scale using one or multiple response options.
+
+# Examples
+```julia-repl
+julia> options = [
+    response_option(code="A1", option="1"),
+    response_option(code="A2", option="2")
+]
+julia> response_scale(options=options, header="my response scale")
+
+```
+"""
 response_scale(; options, header="") = ResponseScale(header, options)
+
+
+"""
+    response_scale(children; header::AbstractString)
+
+Construct a LimeSurvey Response Scale using `do ... end` syntax for response options.
+
+# Examples
+```julia-repl
+julia> response_scale(header="my response scale") do
+    response_option(code="A1", option="1"),
+    response_option(code="A2", option="2")
+end
+```
+"""
 function response_scale(options::Function; header="")
     return ResponseScale(header, tovector(options()))
 end
 
-function point_scale(n)
+"""
+    point_scale(n::Integer)
+
+Construct a `ResponseScale` ranging from `1` to `n`.
+
+# Examples
+julia> point_scale(3)
+"""
+function point_scale(n::Integer)
     options = [response_option(code="A$i", option="$i") for i in 1:n]
     scale = response_scale(options=options)
     return scale
